f5150a7478bcede8f1cc99a52c833fe3be90946a
googol plex <googolplex@lcompras.biz>
2017-10-11 14:26:46 -0300

    elaborando la model

--- rrhhfelizv1/src/biz/lcompras/model/Funciones.java
+++ rrhhfelizv1/src/biz/lcompras/model/Funciones.java
@@ -1,5 +1,54 @@
 package biz.lcompras.model;
 
+import java.util.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+import org.openxava.annotations.*;
+import org.openxava.util.*;
+
+@Entity
+@Table(name="RRHH_FUNCIONES"
+ , uniqueConstraints={
+        @UniqueConstraint(name="FUN_CODIGO_DUPLICADO", columnNames={"FUN_CODIGO"})
+        , @UniqueConstraint(name="FUN_FUNCION_DUPLICADO", columnNames={"FUN_FUNCION"})        
+ }
+)
 public class Funciones extends SuperClaseFeliz {
 
+	@Required
+	@Pattern(regexp="^[0-9]+$",message="No es un numero tipo NNNNNNNNN ")
+	@Column(length=20,nullable=false,name="FUN_CODIGO")		
+	private String funCodigo;
+	@Required
+	@Column(length=80,nullable=false,name="FUN_FUNCION")		
+	private String funFuncion;
+
+	
+	public String getfunCodigo() {
+		return funCodigo;
+	}
+
+	public void setfunCodigo(String funCodigo) {
+		this.funCodigo = funCodigo.toUpperCase().trim();
+	}
+
+	public String getfunFuncion() {
+		return funFuncion;
+	}
+
+
+
+	public void setfunFuncion(String funFuncion) {
+		this.funFuncion = funFuncion.toUpperCase().trim();
+	}
+
+	@PreUpdate
+	private void ultimoPaso() {
+			Date mifechora = new java.util.Date() ;
+			super.setModificadoPor(Users.getCurrent()) ;
+			super.setFchUltMod(mifechora)  ;
+	}		
+	
 }
